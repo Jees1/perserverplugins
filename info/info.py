b1f9d597a453060a411453a6e18496998e7b4f58
Jees1 <49425409+Jees1@users.noreply.github.com>
2019-11-30 08:24:40 +0200

    Create info.py

---
+++ info/info.py
@@ -0,0 +1,31 @@
+import asyncio
+import discord
+from discord.ext import commands
+
+from core import checks
+from core.models import PermissionLevel
+
+
+class info(commands.Cog):
+    """
+    A sample set of rules you can use when your first starting your server to save time.
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=["credits"])
+    @checks.has_permissions(PermissionLevel.REGULAR)
+    async def info(self, ctx):
+        """Send the executives of Drizzle Hotels"""
+        embed = discord.Embed(
+            title="Drizzle Hotel Executives"
+        )
+        embed.set_author(name=str(ctx.author), icon_url=str(ctx.author.avatar_url))
+        embed.description = """hei
+            """
+        embed.color = self.bot.main_color
+        return await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(info(bot))
