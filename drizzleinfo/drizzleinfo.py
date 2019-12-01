e26a0705f93362762766bc396849a34c5f94c06f
Jees1 <49425409+Jees1@users.noreply.github.com>
2019-12-01 10:06:14 +0200

    Create drizzleinfo.py

---
+++ drizzleinfo/drizzleinfo.py
@@ -0,0 +1,40 @@
+import asyncio
+import discord
+from discord.ext import commands
+
+from core import checks
+from core.models import PermissionLevel
+
+
+class drizzleinfo(commands.Cog):
+    """
+    A sample set of rules you can use when your first starting your server to save time.
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=["credits"])
+    @checks.has_permissions(PermissionLevel.REGULAR)
+    async def drizzleinfo(self, ctx):
+        """Send the credits of the bot."""
+        embed = discord.Embed(
+            title="Credits of the bot"
+        )
+        embed.set_author(name=str(ctx.author), icon_url=str(ctx.author.avatar_url))
+        embed.description = """
+                *This bot was setup for Drizzle Hotels for the cause of Moderators communicating with our users privately.*
+
+**Credits:**
+Kyb3r,
+Taki,
+Stephen,
+Piyush,
+DazVise,
+Jees1.
+            """
+        embed.color = self.bot.main_color
+        return await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(drizzleinfo(bot))
