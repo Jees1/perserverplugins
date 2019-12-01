22fa1c182f851b194912174e85178cdd7bdbd4f0
Jees1 <49425409+Jees1@users.noreply.github.com>
2019-12-01 09:30:14 +0200

    Update drizzleranks.py

--- drizzleranks/drizzleranks.py
+++ drizzleranks/drizzleranks.py
@@ -1,4 +1,4 @@
-import asyncio
+Iimport asyncio
 import discord
 from discord.ext import commands
 
@@ -23,11 +23,7 @@ class drizzleRanks(commands.Cog):
         )
         embed.set_author(name=str(ctx.author), icon_url=str(ctx.author.avatar_url))
         embed.description = """
-                {
-    "embed": {
-        "description": "- | Owner __255__\n- | Co-Owner __254__\n- | Development Team __253__\n- | Group Managers __252__\n- | Overseers __251__\nChairperson __250__\nVice-Chairperson __13__\nExecutive __12__\nPublic Relations Officer __11__\nBoard of Directors __10__\nShift Manager __9__\nSupervisor __8__\nSecurity __7__\nReceptionist __6__\nTrainee __5__\nAllied Represantive __4__\n- | Noted Guest __3__\nX | Suspended __2__\nHotel Guest __1__"
-    }
-}
+                - | Owner __255__\n- | Co-Owner __254__\n- | Development Team __253__\n- | Group Managers __252__\n- | Overseers __251__\nChairperson __250__\nVice-Chairperson __13__\nExecutive __12__\nPublic Relations Officer __11__\nBoard of Directors __10__\nShift Manager __9__\nSupervisor __8__\nSecurity __7__\nReceptionist __6__\nTrainee __5__\nAllied Represantive __4__\n- | Noted Guest __3__\nX | Suspended __2__\nHotel Guest __1__"
             """
         embed.color = self.bot.main_color
         return await ctx.send(embed=embed)
