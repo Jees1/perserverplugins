9a589e1653bb5b37dd63515009bea23a8bd7ae43
Jees1 <49425409+Jees1@users.noreply.github.com>
2019-12-01 09:45:01 +0200

    Update info.py

--- info/info.py
+++ info/info.py
@@ -23,7 +23,7 @@ class info(commands.Cog):
         )
         embed.set_author(name=str(ctx.author), icon_url=str(ctx.author.avatar_url))
         embed.description = """
-             Hello
+             *This bot was setup for Drizzle to help the moderation team in communication with our users.*
             """
         embed.color = self.bot.main_color
         return await ctx.send(embed=embed)
