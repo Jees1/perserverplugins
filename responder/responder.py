e00203a12bdd07dceba4d4f0fecf4cfb54c18de7
Jees1 <49425409+Jees1@users.noreply.github.com>
2020-10-04 11:03:38 +0300

    Update responder.py

--- responder/responder.py
+++ responder/responder.py
@@ -3,10 +3,10 @@ from discord.ext import commands
 class MyCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        message.channel.send(message.content)
+@client.event
+async def on_message(message):
+    if "word" in message.content:
+          await message.channel.send("hi")
 
 def setup(bot):
     bot.add_cog(MyCog(bot))
