f5c3acf3ed1669b3f71762c6a9b3f7074bf85dfd
Jees1 <49425409+Jees1@users.noreply.github.com>
2020-10-04 10:51:34 +0300

    Update responder.py

--- responder/responder.py
+++ responder/responder.py
@@ -4,11 +4,9 @@ class MyCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @client.event
-async def on_message(message):
-
-if "wow" in message.content:
-    await message.channel.send("oh wow")
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        await message.channel.send(message.content)
 
 def setup(bot):
     bot.add_cog(MyCog(bot))
