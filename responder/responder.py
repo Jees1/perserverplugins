38be46eee1b77631ebd583a46267d13adf855d0e
Jees1 <49425409+Jees1@users.noreply.github.com>
2020-10-05 17:13:23 +0300

    Update responder.py

--- responder/responder.py
+++ responder/responder.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-import asyncio
+
 class MyCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -8,6 +8,5 @@ class MyCog(commands.Cog):
     async def on_message(self, message):
         if "happy birthday" in message.content.lower():
           await message.channel.send("ye happy birthday")
-          asyncio.sleep(1)
 def setup(bot):
     bot.add_cog(MyCog(bot))
