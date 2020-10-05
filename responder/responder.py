a2b1bb154a28136c1d61022a7805e009f5d7cf52
Jees1 <49425409+Jees1@users.noreply.github.com>
2020-10-05 17:06:45 +0300

    Update responder.py

--- responder/responder.py
+++ responder/responder.py
@@ -3,11 +3,14 @@ from discord.ext import commands
 class MyCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
+cooldown = True
     @commands.Cog.listener()
     async def on_message(self, message):
+     if cooldown == True:
         if "happy birthday" in message.content.lower():
+          cooldown = False
           await message.channel.send("ye happy birthday")
-
+          asyncio.sleep(1)
+          cooldown = True
 def setup(bot):
     bot.add_cog(MyCog(bot))
